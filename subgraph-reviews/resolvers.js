a159ecebbe9b1fbf96211682706fa33a9148f735
Thomas Perron <thomasperron@Thomass-MacBook-Pro-2.local>
2022-03-21 21:09:35 -0400

    Voyage I: Federation from Day One

--- subgraph-reviews/resolvers.js
+++ subgraph-reviews/resolvers.js
@@ -1,15 +1,33 @@
 const resolvers = {
   Query: {
-    latestReviews(_, __, {dataSources}) {
+    latestReviews(_, __, { dataSources }) {
       return dataSources.reviewsAPI.getLatestReviews();
-    }
+    },
+  },
+  Location: {
+    overallRating: ({ id }, _, { dataSources }) => {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+    reviews: ({ id }, _, { dataSources }) => {
+      return dataSources.reviewsAPI.getReviewsForLocation(id);
+    },
   },
   Mutation: {
-    submitReview(_, {review}, {dataSources}) {
+    submitReview(_, { review }, { dataSources }) {
       const newReview = dataSources.reviewsAPI.submitReviewForLocation(review);
-      return {code: 200, success: true, message: 'success', review: newReview};
-    }
-  }
+      return {
+        code: 200,
+        success: true,
+        message: "success",
+        review: newReview,
+      };
+    },
+  },
+  Review: {
+    location({ locationId }) {
+      return { id: locationId };
+    },
+  },
 };
 
 module.exports = resolvers;
